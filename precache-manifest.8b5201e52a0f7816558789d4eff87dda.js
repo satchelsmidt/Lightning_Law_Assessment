6db096eac97c43008ae0416e18fb4bc872730f69
satchelsmidt <satchelsmidt@gmail.com>
2020-08-07 16:53:46 -0700

    Updates

---
+++ precache-manifest.8b5201e52a0f7816558789d4eff87dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1fc26e75be495fc2d9e216050e91af",
+    "url": "/Lightning_Law_Assessment/index.html"
+  },
+  {
+    "revision": "7152ef021d22e4c511f9",
+    "url": "/Lightning_Law_Assessment/static/css/main.f0397217.chunk.css"
+  },
+  {
+    "revision": "43d9a3fee4001707fe17",
+    "url": "/Lightning_Law_Assessment/static/js/2.2cee87c6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Lightning_Law_Assessment/static/js/2.2cee87c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7152ef021d22e4c511f9",
+    "url": "/Lightning_Law_Assessment/static/js/main.aa8b56ec.chunk.js"
+  },
+  {
+    "revision": "0ea2c2e12890f7ef38c0",
+    "url": "/Lightning_Law_Assessment/static/js/runtime-main.fd35c18f.js"
+  }
+]);
